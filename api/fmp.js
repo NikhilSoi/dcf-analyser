3246d792a9fd0c7c916266afb714c0ea638f5cf5
NikhilSoi <nikhil.soi89@gmail.com>
2026-03-05 20:31:03 +0000

    hardcode api key in serverless

--- api/fmp.js
+++ api/fmp.js
@@ -1,7 +1,9 @@
 export default async function handler(req, res) {
   const { path, ...params } = req.query;
-  const apikey = process.env.REACT_APP_FMP_KEY || process.env.FMP_KEY;
-  const query = new URLSearchParams({ ...params, apikey }).toString();
+  const query = new URLSearchParams({
+    ...params,
+    apikey: "Ej79htMdNaKHnInR1Rc1Imk7O1ublSCB"
+  }).toString();
   const url = `https://financialmodelingprep.com${path}?${query}`;
   const response = await fetch(url);
   const data = await response.json();
